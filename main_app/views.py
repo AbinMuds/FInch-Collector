1caa41c415577aa30b841fd27e3f7b38896f28ee
ABHI <am@MacBook-Pro.local>
2021-03-10 05:08:35 -0800

    adds Dogs models and also views and templates to show all the data that is associated with all the dogs

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Dog
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def data(request):
+    return render(request, 'data.html')
+
+def dogs_index(request):
+    dogs = Dog.objects.all()
+    return render(request, 'dog/index.html', {'dogs':dogs})
+
+
+
